5b51a6ba647426e226073319e1189c4e62752355
nerxid <134153856+nerxid@users.noreply.github.com>
2024-08-02 12:45:06 +0700

    edit title

--- unit/manage-unit.php
+++ unit/manage-unit.php
@@ -17,7 +17,7 @@ $unitidList=$unitnameList=array();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Status</title>
+    <title>Unit</title>
 </head>
 <body>
 
